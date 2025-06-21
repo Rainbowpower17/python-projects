e938afbaa34312de81e21199ba1a0150759ace51
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-06-21 08:52:24 +0300

    Add files via upload

---
+++ Calculations.py
@@ -0,0 +1,36 @@
+print("Hello and welcome to your test")
+print("Ok lets start")
+A=5
+B=6
+C=7
+D=8
+E=1
+F=2
+G=3
+H=4
+I=13
+J=14
+K=15
+L=16
+M=9
+N=10
+O=11
+P=12
+Q=21
+R=22
+S=23
+T=24
+U=17
+V=18
+W=19
+X=20
+Y=26
+Z=25
+print("now its time for the Test")
+print(A+G)
+print(W**X)
+print(Q//U)
+print(T**B)
+print(T-D)
+print(Z-W)
+print(S+Y)
