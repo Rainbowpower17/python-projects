ec8b90a7a3d7fadd91e825bcfcbff4c71a2d3209
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-06-28 14:45:39 +0300

    Add files via upload

---
+++ Natural number.py
@@ -0,0 +1,9 @@
+A=0,1,2,3,4
+B=1,2,3,4,
+W=int(input("Enter your number"))
+if W==A:
+    print("This is a natuaral number")
+else:
+    print("This is a counting number")
+if W==A and B:
+    print("This is both a natural and counting number")
