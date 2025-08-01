56b24a68a0782aac9888828cd7fdceee707b7c5d
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-08-01 08:54:46 -0700

    Update slam book.py

---
+++ slam book.py
@@ -0,0 +1,9 @@
+slam_book={}
+def add_hobby():
+    name=input("Enter your name: ").strip()
+    hobby=input("Enter your hobby: ").strip()
+    print(f"Hobby '{name}' added")
+def main():
+    print("1.Add hobby")
+    print("2.Exit")
+main()         
