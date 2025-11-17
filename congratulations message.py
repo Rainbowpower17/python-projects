36f767071b2577103a25855c4a600d114adb39f6
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-11-17 08:01:16 -0800

    Add files via upload

---
+++ congratulations message.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+revname=name[::-1]
+print("Congratulations",name)
+print("The Datatype of name is :",type(name))
+print("The Reverse of this string is :")
+print(name)
