58fbcd3b5d7206f6c289443f84115df89ee8bbc5
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-11-21 07:22:47 -0800

    Add files via upload

---
+++ Caculating precentages.py
@@ -0,0 +1,12 @@
+math=98
+science=98
+amharic=95
+pva=98
+afa=98
+english=100
+geb=98
+pe=98
+sum=math=geb+pe+pva+english+amharic+afa+science
+print('The sum is :', sum)
+perc=(sum/800)*100
+print('Precentage is :', perc)
