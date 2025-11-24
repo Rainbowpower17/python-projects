871f80d8d45e9ec99819dd74719e8ab841defc96
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-11-24 07:17:29 -0800

    Add files via upload

---
+++ Checking if number is grater or smaller.py
@@ -0,0 +1,9 @@
+i=int(input("Enter a number: "))
+
+if (i<5):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print("i is greater than or equal to 15")
+    print("i'm in else block")
+print("i'm not in if and else block")    
