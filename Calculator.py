763ac219dbea32ada38681173dd599cedfd8e5d3
Rainbowpower17 <abesalatbiruk@gmail.com>
2025-12-29 07:26:19 -0800

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,34 @@
-F=int(input("Enter your number"))
-G=int(input("Enter your second number"))
-print(F+G)
+def add(p,q):
+    return p+q
+
+def subtract(p,q):
+    return p-q
+
+def divide(p,q):
+    return p/q
+
+def multiply(p,q):
+    return p*q
+
+print('Please select the operation')
+print('a.Add')
+print('b.Subtract')
+print('c.Multiply')
+print('d.Divide')
+
+choice=input('Please enter choice(a/b/c/d)')
+
+num_1=int(input("Enter the first number"))
+num_2=int(input("Enter the second number"))
+
+if choice =='a':
+    print(num_1,'+',num_2,'=', add(num_1,num_2))
+elif choice=='b':
+    print(num_1,'-',num_2,'=', subtract(num_1,num_2))
+elif choice=='c':
+    print(num_1,'*',num_2,'=', multiply(num_1,num_2))
+elif choice=='d':
+    print(num_1,'/',num_2,'=', divide(num_1,num_2))
+
+else:
+    print("This is an invalid input")
