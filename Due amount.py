5bce11f329856cfb8ba08dda9e5fb045fd72ce66
Rainbowpower17 <abesalatbiruk@gmail.com>
2026-01-05 07:31:01 -0800

    Add files via upload

---
+++ Due amount.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the amount :"))
+b=int(input("Enter the amount payed:"))
+
+def due_Amount():
+    c=a-b
+    print("The amount you have left to pay is:",c)
+
+
+
+
+
+
+
+due_Amount()
