3b625431c419ec947d72fb3f98d9e4ccae6a03cc
Rainbowpower17 <abesalatbiruk@gmail.com>
2026-02-27 07:09:06 -0800

    Add vehicle and Bus class with instantiation

---
+++ is this dad.py
@@ -0,0 +1,16 @@
+class veichle:
+
+
+    def __init__(self,name,max_speed,mileage):
+        self.name=name
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+
+class Bus(veichle):
+    pass
+
+
+
+School_bus=Bus("School Volvo",180,12)
+print("Vehicle Name:",School_bus.name,"Speed:",School_bus.max_speed,"Mileage:",School_bus.mileage)
