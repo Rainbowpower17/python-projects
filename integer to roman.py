24313e77c2085fd8b82dcef969d9acfb3f3edec6
Rainbowpower17 <abesalatbiruk@gmail.com>
2026-03-26 09:21:11 -0700

    Add class for converting Roman numerals to integers

---
+++ integer to roman.py
@@ -0,0 +1,19 @@
+class number:
+    num=1
+    num2=2
+    num3=3
+    num4=4
+    num5=5
+    roman_num="I"
+    roman_num2="II"
+    roman_num3="III"
+    roman_num4="IIII"
+    roman_num5="V"
+ob=number
+
+print("Roman mubers converted to Regular numbers(1-5)")
+print(ob.roman_num ,"is equal to",ob.num)
+print(ob.roman_num2 ,"is equal to",ob.num2)
+print(ob.roman_num3 ,"is equal to",ob.num3)
+print(ob.roman_num4 ,"is equal to",ob.num4)
+print(ob.roman_num5 ,"is equal to",ob.num5)
